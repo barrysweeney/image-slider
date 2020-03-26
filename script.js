e852f113f532aed0359ba5e8c5f8731369d1275f
barrysweeney <barrysweeney172@live.co.uk>
2020-03-26 16:59:43 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const leftPane = document.getElementById("left-pane");
+const imagePane = document.getElementById("image-pane");
+const rightPane = document.getElementById("right-pane");
+const progressPane = document.getElementById("progress-pane");
+
+const images = ["planets.jpg", "spaceship.jpg", "wormhole.png"];
